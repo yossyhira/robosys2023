236804fa3027dc2351ab6857c386c5ff29ac9a27
yossy <hirata@hogehoge.com>
2023-10-26 20:57:25 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -xv 
 # SPDX-FileCopyrightText: 2023 Yoshitaka Hirata
 # SPDX-License-Identifier: BSD-3-clause
 ng () {
@@ -12,8 +12,17 @@ out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng ${LINENO}
 
-[ "$ret" = 0 ] && echo OK
+### 変なインプット###
 
+out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+[ "$ret" = 0 ] && echo OK
 exit $ret
 
 
