664123e47c77ddb318eddea9cce967d2859f8691
yossy <hirata@hogehoge.com>
2023-10-26 12:56:14 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -1,7 +1,21 @@
 #!/usr/bin/bash
+# SPDX-FileCopyrightText: 2023 Yoshitaka Hirata
+# SPDX-License-Identifier: BSD-3-clause
+ng () {
+	echo NG at Line ${1}
+	ret=1
+}
+
+ret=0
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$ret" = 0 ] && echo OK
+
+exit $ret
+
+
 
 
