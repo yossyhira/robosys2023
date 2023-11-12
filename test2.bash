c1f8f90d428aaeec6f7f13e3cd0fa6c669b2302a
yossy <hirata@hogehoge.com>
2023-11-12 19:31:46 +0900

    add test2.bash

---
+++ test2.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/bash -xv 
+# SPDX-FileCopyrightText: 2023 Yoshitaka Hirata
+# SPDX-License-Identifier: BSD-3-clause
+ng () {
+	echo NG at Line ${1}
+	ret=1
+}
+
+ret=0
+
+out=$(./japanes_holiday 1/1)
+
+[ "${out}" = 元日、鉄腕アトムの日 ] || ng ${LINENO}
+
+[ "$ret" = 0 ] && echo OK
+exit $ret
